11507ca6e49d654b425d33a4cd9bd1b8d857349f
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-05-30 23:47:29 -0400

    testing

---
+++ build/precache-manifest.b2636c10111d59e822867f460801d5da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbcf93b15bad29e052b9417dc7b023e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "507d58f9016c437c8cad",
+    "url": "/static/css/main.9f45c89d.chunk.css"
+  },
+  {
+    "revision": "d3a9d78323ee68531854",
+    "url": "/static/js/2.36b54eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36b54eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507d58f9016c437c8cad",
+    "url": "/static/js/main.4de68be4.chunk.js"
+  },
+  {
+    "revision": "db567ff74eca65429079",
+    "url": "/static/js/runtime-main.f3bbea13.js"
+  }
+]);
